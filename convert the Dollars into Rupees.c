c99dd96144b6e8645c18bf63805a3fc9a9b65f12
Srinithinv <149354156+Srinithinv@users.noreply.github.com>
2025-09-29 11:29:35 +0530

    Add files via upload

---
+++ convert the Dollars into Rupees.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+int a,b;
+printf("Enter amount in Dollars\n");
+scanf("%d",&a );
+b=a*71.16;
+printf("Total amount %d in Rupees from %d Dollars\n",b,a);
+return 0;
+}
