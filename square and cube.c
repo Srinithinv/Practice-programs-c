b4c18e9ddf40cf1c6aa85ae6c2571c88782ac752
Srinithinv <149354156+Srinithinv@users.noreply.github.com>
2025-09-29 09:52:59 +0530

    Add files via upload

---
+++ square and cube.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+int a, c, d;
+printf("Enter A number:\n");
+scanf("%d",&a);
+c=a*a;
+d=a*a*a;
+printf("Square of number %d is = %d \n",a,c);
+printf("Cube of Number %d is = %d\n",a,d);
+return 0;
+}
