e4264e4e1c10dfb869e03b6e487bd671521a09fd
Srinithinv <149354156+Srinithinv@users.noreply.github.com>
2025-09-29 11:36:13 +0530

    Add files via upload

---
+++ find Area of Triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+int b,h,area;
+printf("Enter base and height of triangle\n");
+scanf("%d%d",&b,&h);
+area=(b*h)/2;
+printf("Area of the triangle is %d ",area);
+return 0;
+}
