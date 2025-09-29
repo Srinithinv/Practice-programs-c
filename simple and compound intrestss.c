cd5e575c6221ddbee024fcf600d403e43bb23efd
Srinithinv <149354156+Srinithinv@users.noreply.github.com>
2025-09-29 10:45:58 +0530

    Add files via upload

---
+++ simple and compound intrestss.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int p,t;
+float r,si,amount,ci;
+printf("Please enter principle,time and rate of interest\n");
+scanf("%d%d%f",&p,&t,&r);
+si=p*t*r/100;
+printf("\nSimple interest = %.3f",si);
+amount=p*pow((1 +r/100),t);
+ci=amount-p;
+printf("\nCompound interest = %.3f",ci);
+return 0;
+}
+
