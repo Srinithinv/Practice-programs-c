680ecacac9c76d5590c0c354d6bf4d645e4ded4d
Srinithinv <149354156+Srinithinv@users.noreply.github.com>
2025-09-29 10:00:00 +0530

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+int a,b,c;
+printf("Enter length of the square side\n");
+scanf ("%d",&a);
+b=a*a;
+c=4*a;
+printf("Area of square = %d\n",b);
+printf("Perimeter of square= %d\n",c);
+return 0;
+}
